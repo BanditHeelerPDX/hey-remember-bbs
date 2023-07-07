520c4855ebca97e1ab5d1badfbfbe7f9b849a979
Satalae <harrison19988@gmail.com>
2023-07-06 18:25:26 -0700

    rebranch

---
+++ client/src/pages/home.js
@@ -0,0 +1,19 @@
+// Imports
+
+
+const homePage = () => {
+    //Logic needed for login
+
+    // Fully constructed homepage
+    // Will change depending on loggedIn state
+    return (
+        /* Main Div */
+        <div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default homePage;
