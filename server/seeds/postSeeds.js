56b114ffdd6d47598730647091d3275c7eec085f
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-11 11:25:04 -0700

    seed data, dependency update

---
+++ server/seeds/postSeeds.js
@@ -0,0 +1,71 @@
+const posts = [
+  {
+    _id: "1",
+    postText: "I am the king of soul",
+    userId: "1",
+    postDate: "2021-09-01T18:25:43.511Z",
+    comments: [
+      {
+        _id: "1",
+        commentText: "No, I am the king of soul",
+        commentAuthor: "2",
+        commentDate: "2021-09-01T18:25:43.511Z",
+      },
+      {
+        _id: "2",
+        commentText: "No, I am the king of soul",
+        commentAuthor: "3",
+        commentDate: "2021-09-01T18:25:43.511Z",
+      },
+    ],
+  },
+  {
+    _id: "2",
+    postText: "I have been around the world in less than 180 days.",
+    userId: "2",
+    postDate: "2021-09-01T18:25:43.511Z",
+    comments: [
+      {
+        _id: "1",
+        commentText: "No, I have been around the world in less than 180 days.",
+        commentAuthor: "1",
+        commentDate: "2021-09-01T18:25:43.511Z",
+      },
+      {
+        _id: "2",
+        commentText: "No, I have been around the world in less than 180 days.",
+        commentAuthor: "3",
+        commentDate: "2021-09-01T18:25:43.511Z",
+      },
+    ],
+  },
+  {
+    _id: "3",
+    postText:
+      "My copilot, Phil has really lost his touch lately.  What is wrong with you, Phil? Do you need to speak to someone?",
+    userId: "3",
+    postDate: "2021-09-01T18:25:43.511Z",
+    comments: [
+      {
+        _id: "1",
+        commentText: "Yeah, he really shit the bed.",
+        commentAuthor: "5",
+        commentDate: "2021-09-13T09:16:34.529Z",
+      },
+    ],
+  },
+  {
+    _id: "4",
+    postText: "I wanna know.  Have you ever seen the rain?",
+    userId: "9",
+    postDate: "2020-09-13T09:16:34.529Z",
+    comments: [
+      {
+        _id: "1",
+        commentText: "Are you serious with this? Who hasn't seen the rain?",
+        commentAuthor: "6",
+        commentDate: "2021-01-02T08:07:59.623Z",
+      },
+    ],
+  },
+];
