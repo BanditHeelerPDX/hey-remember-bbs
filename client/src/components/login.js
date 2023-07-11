43f4c46a0a02c3a080f5fcc053fd20b6d6b65df9
Satalae <harrison19988@gmail.com>
2023-07-11 16:02:31 -0700

    login commit

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,6 +2,32 @@ import React, { useState } from 'react';
 
 const login = () => {
 
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const field = e.target;
+        const fieldData = new field 
+    }
+
+    return(
+        <div>
+            <div className="login">
+                <h2>Log-in</h2>
+                <h3>Username:</h3>
+                <input
+                type="text" 
+                value={this.state.value} 
+                >
+                
+                </input>
+                <h3>Password:</h3>
+
+            </div>
+            <div className="signup">
+
+            </div>
+        </div>
+    );
 }
 
 export default login;
