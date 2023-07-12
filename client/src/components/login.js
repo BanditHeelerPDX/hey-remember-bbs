67ecf9fc37267925a9a77829622436e49bca8c38
Satalae <harrison19988@gmail.com>
2023-07-11 17:03:01 -0700

    login finished

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,29 +3,60 @@ import React, { useState } from 'react';
 const login = () => {
 
     function handleSubmit(e){
+        // Prevent browser from loading new page
         e.preventDefault();
 
+        // Reads the given form's data
         const field = e.target;
-        const fieldData = new field 
+        const fieldData = new fieldData(field);
+
+        // Send away here depending on method
     }
 
     return(
         <div>
-            <div className="login">
+            <form method="get" onSubmit={handleSubmit}>
                 <h2>Log-in</h2>
-                <h3>Username:</h3>
+                <label htmlFor='login-username'>Username: </label>
                 <input
                 type="text" 
                 value={this.state.value} 
+                id='login-username'
                 >
-                
                 </input>
-                <h3>Password:</h3>
 
-            </div>
-            <div className="signup">
+                <label htmlFor='login-password'>Password: </label>
+                <input
+                type='text'
+                value={this.state.value}
+                id='login-password'
+                >
+                </input>
 
-            </div>
+            </form>
+            <form method="post" onSubmit={handleSubmit}>
+                <h2>Sign-up</h2>
+                <label htmlFor='signup-username'>Username: </label>
+                <input
+                type='text'
+                value={this.state.value}
+                id='signup-username'>
+                </input>
+
+                <label htmlFor='signup-password'>Password: </label>
+                <input
+                type='text'
+                value={this.state.value}
+                id='signup-password'>  
+                </input>
+
+                <label htmlFor='signup-email'>Email: </label>
+                <input
+                type='text'
+                value={this.state.value}
+                id='signup-email'>  
+                </input>
+            </form>
         </div>
     );
 }
