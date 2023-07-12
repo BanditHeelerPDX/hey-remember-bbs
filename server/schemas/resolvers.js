4cb8ba4d8cd4d28c718f850fb741d954c39f35d5
Patrick Muth <marinermuph@gmail.com>
2023-07-11 17:33:33 -0700

    PostList and PostForm components added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User, Post, Friend } = require("../models");
+const { User, Post } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
