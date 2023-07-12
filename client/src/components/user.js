2db0d4fcc6c0f4b8dcb82fadfa375ee0f33afa28
Patrick Muth <marinermuph@gmail.com>
2023-07-11 18:40:50 -0700

    refactored user and deleted BC comp

--- client/src/components/user.js
+++ client/src/components/user.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const User = ({ user }) => {
-    const { username, firstName, lastName, email } = user;
+    const { username, email } = user;
   
     return (
       <div className="user-card">
         <h3>{username}</h3>
-        <p>First Name: {firstName}</p>
-        <p>Last Name: {lastName}</p>
         <p>Email: {email}</p>
       </div>
     );
