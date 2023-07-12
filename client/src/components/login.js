d1d08b808e9e9119ff5ef726514c75c69a83de3d
Anthony-A-Perez <ap.erez@me.com>
2023-07-11 19:06:01 -0700

    login styling

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,64 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const login = () => {
+  function handleSubmit(e) {
+    // Prevent browser from loading new page
+    e.preventDefault();
 
-    function handleSubmit(e){
-        // Prevent browser from loading new page
-        e.preventDefault();
+    // Reads the given form's data
+    const field = e.target;
+    const fieldData = new fieldData(field);
 
-        // Reads the given form's data
-        const field = e.target;
-        const fieldData = new fieldData(field);
+    // Send away here depending on method
+  }
 
-        // Send away here depending on method
-    }
+  return (
+    <div className="container">
+      <div className="card bg-purple-dark-7 o-90">
+        <h2 className='card-title text-orange ml-3"'>Log-in</h2>
+        <div className="card-body">
+          <form method="get" onSubmit={handleSubmit}>
+            <label htmlFor="login-username">Username: </label>
+            <input
+              className="card col-12-xs mb-2 bg-purple-light-9"
+              placeholder="Your username"
+              type="text"
+              value={this.state.value}
+              id="login-username"
+            ></input>
 
-    return(
-        <div>
-            <form method="get" onSubmit={handleSubmit}>
-                <h2>Log-in</h2>
-                <label htmlFor='login-username'>Username: </label>
-                <input
-                type="text" 
-                value={this.state.value} 
-                id='login-username'
-                >
-                </input>
-
-                <label htmlFor='login-password'>Password: </label>
-                <input
-                type='text'
-                value={this.state.value}
-                id='login-password'
-                >
-                </input>
-
-            </form>
-            <form method="post" onSubmit={handleSubmit}>
-                <h2>Sign-up</h2>
-                <label htmlFor='signup-username'>Username: </label>
-                <input
-                type='text'
-                value={this.state.value}
-                id='signup-username'>
-                </input>
-
-                <label htmlFor='signup-password'>Password: </label>
-                <input
-                type='text'
-                value={this.state.value}
-                id='signup-password'>  
-                </input>
-
-                <label htmlFor='signup-email'>Email: </label>
-                <input
-                type='text'
-                value={this.state.value}
-                id='signup-email'>  
-                </input>
-            </form>
+            <label htmlFor="login-password">Password: </label>
+            <input
+              className="card col-12-xs mb-2 bg-purple-light-9"
+              placeholder="******"
+              type="text"
+              value={this.state.value}
+              id="login-password"
+            ></input>
+            <button
+              className="btn-outlined-orange text-purple text-hover-white"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-    );
-}
+      </div>
+      <div className="card bg-purple-dark-7 o-90">
+        <h2 className="card-title text-orange ml-3">Sign-up</h2>
+        <div className="card-body flex-row justify-center mb-2">
+          <form method="post" onSubmit={handleSubmit}>
+            <label htmlFor="signup-username">Username: </label>
+            <input
+              className="card col-12-xs mb-2 bg-purple-light-9"
+              placeholder="Your username"
+              type="text"
+              value={this.state.value}
+              id="signup-username"
+            ></input>
+
+            <label htmlFor="signup-password">Password: </label>
+            <input
+              className="card col-12-xs mb-2 bg-purple-light-9"
+              placeholder="******"
+              type="text"
+              value={this.state.value}
+              id="signup-password"
+            ></input>
+
+            <label htmlFor="signup-email">Email: </label>
+            <input
+              className="card col-12-xs mb-2 bg-purple-light-9"
+              placeholder="Your email"
+              type="text"
+              value={this.state.value}
+              id="signup-email"
+            ></input>
+            <button
+              className="btn-outlined-orange text-purple text-hover-white"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default login;
