10065985d8f4c4aa8adea82e46c8cc825e0e1f96
Anthony-A-Perez <ap.erez@me.com>
2023-07-11 19:08:49 -0700

    post list styling

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -12,26 +12,27 @@ const PostList = () => {
   const posts = data.posts;
 
   return (
-    <div>
-      <h2>Posts</h2>
+    <div className="card bg-purple-light-9 m-2 pt-2 pb-4 br-lg text-purple-dark-7">
+      <h2 className="card-title">Posts</h2>
       {posts.length === 0 ? (
-        <p>No posts found.</p>
+        <p className="card-body">No posts found.</p>
       ) : (
-        <div>
+        <div className="card bg-purple-light-9 m-2 pt-2 pb-4 br-lg text-purple-dark-7">
           {posts.map((post) => (
-            <div key={post._id} className="post">
+            <div key={post._id} className="post card-body">
               <h3>{post.postText}</h3>
               <p>Posted by: {post.userId}</p>
               <p>Date: {new Date(post.postDate).toLocaleString()}</p>
-
-              <h4>Comments:</h4>
+              <div className="container">
+              <h4 className="mt-2">Comments:</h4>
               {post.comments.map((comment) => (
-                <div key={comment._id} className="comment">
-                  <p>{comment.commentText}</p>
+                <div key={comment._id} className="comment card bg-orange-light-2">
+                  <p className="card-body">{comment.commentText}</p>
                   <p>Posted by: {comment.commentAuthor.username}</p>
                   <p>Date: {new Date(comment.commentDate).toLocaleString()}</p>
                 </div>
               ))}
+              </div>
             </div>
           ))}
         </div>
