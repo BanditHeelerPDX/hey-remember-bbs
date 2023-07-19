393cb93663a1d233bd7db7fd3e1f28dc66461965
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-19 16:56:45 -0700

    debugging errors and updating queries and mutations - invalid hook call error resolved

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -2,26 +2,30 @@ import React from "react";
 
 const CommentList = ({ comments = [] }) => {
   if (!comments.length) {
-    return <p className="card bg-blue-dark-4 o-90 br-lg text-orange-light-3">No comments found.</p>;
+    return (
+      <p className="card bg-blue-dark-4 o-90 br-lg text-orange-light-3">
+        No comments found.
+      </p>
+    );
   }
 
   return (
     <>
-    <div className="container mt-2">
-      <div className="card bg-blue-dark-4 o-90 br-lg">
-      <h4 className="card-title text-orange-light-3 ml-3">Comments:</h4>
-      {comments.map((comment) => (
-        <div key={comment._id} className="comment card bg-blue-light-9  mb-1">
-
-          <p className="font-lg">{comment.commentText}</p> <br/>
-          <p className="font-md">Posted by: {comment.commentAuthor}</p>
-          <p className="font-md">Date: {comment.created}</p>
-          
-          
+      <div className="container mt-2">
+        <div className="card bg-blue-dark-4 o-90 br-lg">
+          <h4 className="card-title text-orange-light-3 ml-3">Comments:</h4>
+          {comments.map((comment) => (
+            <div
+              key={comment._id}
+              className="comment card bg-blue-light-9  mb-1"
+            >
+              <p className="font-lg">{comment.commentText}</p> <br />
+              <p className="font-md">Posted by: {comment.commentAuthor}</p>
+              <p className="font-md">Date: {comment.createdAt}</p>
+            </div>
+          ))}
         </div>
-      ))}
       </div>
-    </div>
     </>
   );
 };
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,46 +1,49 @@
 const { model, Schema } = require("mongoose");
 const User = require("./User");
 
-const PostSchema = new Schema({
-  postAuthor: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  postText: {
-    type: String,
-    required: [true, "Empty posts are not allowed"],
-    minlength: [1, "Posts must be at least 1 character long"],
-    maxlength: [225, "Posts cannot be longer than 225 characters"],
-    trim: true,
-  },
-  postDate: {
-    type: Date,
-    default: Date.now,
-  },
-  // likes: {
-  //   type: Array,
-  //   default: [],
-  // },
-  comments: [
-    {
-      commentText: {
-        type: String,
-        required: [true, "Empty comments are not allowed"],
-        minlength: [1, "Comments must be at least 1 character long"],
-        maxlength: [225, "Comments cannot be longer than 225 characters"],
-        trim: true,
-      },
-      commentAuthor: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-      commentDate: {
-        type: Date,
-        default: Date.now,
-      },
+const PostSchema = new Schema(
+  {
+    postAuthor: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-  ],
-});
+    postText: {
+      type: String,
+      required: [true, "Empty posts are not allowed"],
+      minlength: [1, "Posts must be at least 1 character long"],
+      maxlength: [225, "Posts cannot be longer than 225 characters"],
+      trim: true,
+    },
+    postDate: {
+      type: Date,
+      default: Date.now,
+    },
+    // likes: {
+    //   type: Array,
+    //   default: [],
+    // },
+    comments: [
+      {
+        commentText: {
+          type: String,
+          required: [true, "Empty comments are not allowed"],
+          minlength: [1, "Comments must be at least 1 character long"],
+          maxlength: [225, "Comments cannot be longer than 225 characters"],
+          trim: true,
+        },
+        commentAuthor: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        commentDate: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Post = model("Post", PostSchema);
 
