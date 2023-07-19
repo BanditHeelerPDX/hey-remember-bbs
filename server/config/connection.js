71488d9b5d85a7e883de26d9f9664019b7da9025
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-18 23:42:42 -0700

    matching mutations to typeDefs and resolvers

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,7 @@
-
 const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:/phonebook")
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sea-tec-astronomy"
+);
 
 module.exports = mongoose.connection;
-
-
