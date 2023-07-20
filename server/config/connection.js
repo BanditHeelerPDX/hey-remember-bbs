9375f30282ae3b9b9639a83ea85048dc0612215e
Anthony-A-Perez <ap.erez@me.com>
2023-07-20 11:28:54 -0700

    added COMPASS env variable

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/sea-tec-astronomy"
-);
+mongoose.connect(process.env.MONGODB_URI || process.env.COMPASS);
 
 module.exports = mongoose.connection;
